2d144ad4a4722783f56a28e2b380e2a3ee9b6050
Victor Pryazhnikov <victor@pryazhnikov.com>
2025-06-02 19:32:29 +0100

    processing methods extraction at DirectoryScanner

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -29,37 +29,26 @@ func NewDirectoryScanner(logger zerolog.Logger, checker FileChecker) *DirectoryS
 func (ds *DirectoryScanner) Scan(rootPath string) error {
 	ds.logger.Info().Msgf("Starting directory scan: %s", rootPath)
 
-	counter := 0
+	filesCnt := 0
+	errCnt := 0
 	err := filepath.WalkDir(rootPath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
 		if d.IsDir() {
-			// Nothing to do here
-			ds.logger.Debug().
-				Str("path", path).
-				Msg("Directory found")
-		} else {
-			ds.logger.Debug().
-				Str("path", path).
-				Msg("File found")
-			checkRes, err := ds.checker.Check(path)
-			if err != nil {
-				ds.logger.Warn().
-					Str("path", path).
-					Msgf("Cannot check file: %v", err)
-			} else {
-				ds.logger.Debug().
-					Str("path", path).
-					Str("hash", checkRes).
-					Msg("File was checked")
-			}
+			return ds.processDirectory(path)
+		}
 
-			counter++
-			if counter%summaryPeriod == 0 {
-				ds.logger.Info().Msgf("%d files processed...", counter)
-			}
+		// todo: to implement processing retry later
+		err = ds.processFile(path)
+		if err != nil {
+			errCnt++
+		}
+
+		filesCnt++
+		if filesCnt%summaryPeriod == 0 {
+			ds.logger.Info().Msgf("%d files processed, errors: %d...", filesCnt, errCnt)
 		}
 
 		return nil
@@ -68,6 +57,34 @@ func (ds *DirectoryScanner) Scan(rootPath string) error {
 	if err != nil {
 		return fmt.Errorf("failed to scan directory: %w", err)
 	}
+
+	return nil
+}
+
+func (ds *DirectoryScanner) processDirectory(path string) error {
+	ds.logger.Debug().
+		Str("path", path).
+		Msg("Directory found, nothing to do here.")
+	return nil
+}
+
+func (ds *DirectoryScanner) processFile(path string) error {
+	ds.logger.Debug().
+		Str("path", path).
+		Msg("File found, the check is expected")
+
+	checkRes, err := ds.checker.Check(path)
+	if err != nil {
+		ds.logger.Warn().
+			Str("path", path).
+			Msgf("Cannot check file: %v", err)
+		return err
+	}
+
+	ds.logger.Debug().
+		Str("path", path).
+		Str("hash", checkRes).
+		Msg("File was checked")
 
 	return nil
 }
